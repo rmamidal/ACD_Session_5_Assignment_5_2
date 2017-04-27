c518fa4668503437f19dba03f246734f9281bcd9
Rajendhar Mamidala <rajendhar.mamidala@capgemini.com>
2017-04-27 11:13:13 +0530

    Adding file for Assignment 5.2

---
+++ app.js
@@ -0,0 +1,13 @@
+// Declare and define Object in literal notation.
+var myInfo = {
+  name: 'Rajendhar Mamidala',
+  age : 31,
+  dateOfBirth: '25/10/1985',
+  birthPlace: 'AndhraPradesh',
+}
+
+// Print object properties in console.
+console.log(myInfo.name);
+console.log(myInfo.age);
+console.log(myInfo.dateOfBirth);
+console.log(myInfo.birthPlace);
